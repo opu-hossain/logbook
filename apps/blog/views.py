b09a05df2dd6f75e345d33e6687a1f639b14d200
opu <opu.hossain.data@gmail.com>
2026-05-22 23:20:13 +0600

    feat: added grid post view for the home page

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "home.html")
+    posts = Post.objects.filter(status=Post.Status.PUBLISHED).order_by("-created_at")
+    return render(request, "home.html", {"posts": posts})
