5bf761ab19a5aea6177c8ffcd2cb79dd50357bba
opu <opu.hossain.data@gmail.com>
2026-05-22 16:16:28 +0600

    fix: fixed wrong import in apps/blog/admin.py & phase 1 completed

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from .models import .
+from .models import Post, Category
 
 # Register models here.
 
+
 class PostAdmin(admin.ModelAdmin):
-    list_display= ('title', 'author', 'status', 'category', 'created_at', 'updated_at')
-    list_filter = ('status', 'category')
-    search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)}
+    list_display = ("title", "author", "status", "category", "created_at", "updated_at")
+    list_filter = ("status", "category")
+    search_fields = ("title", "body")
+    prepopulated_fields = {"slug": ("title",)}
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
-
