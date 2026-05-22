17aa403cd4b5ac4b6edc80a8fdfb642e97ed260b
opu <opu.hossain.data@gmail.com>
2026-05-22 16:07:54 +0600

    chore: configured media file serving for development

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def testView(request):
@@ -33,4 +35,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", testView),
     path("htmx-test/", htmxTest),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
