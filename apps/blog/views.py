3d01feba6b0ee92c1bb4ae4b04fe22cb3107f366
opu <opu.hossain.data@gmail.com>
2026-05-25 12:01:45 +0600

    feat: added category filter view

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,6 @@
 from django.core import paginator
 from django.shortcuts import get_object_or_404, render
-from .models import Post
+from .models import Category, Post
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -18,7 +18,23 @@ def post_detail(request, slug):
 
 def blog(request):
     posts = Post.objects.filter(status=Post.Status.PUBLISHED).order_by("-created_at")
+
+    category_slug = request.GET.get("category")
+    active_category = None
+    if category_slug:
+        active_category = get_object_or_404(Category, slug=category_slug)
+        posts = posts.filter(category=active_category)
+    categories = Category.objects.all()
+
     paginator = Paginator(posts, 6)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    return render(request, "blog.html", {"page_obj": page_obj})
+    return render(
+        request,
+        "blog.html",
+        {
+            "page_obj": page_obj,
+            "categories": categories,
+            "active_category": active_category,
+        },
+    )
